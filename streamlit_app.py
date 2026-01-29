7391b09ea1c9a30e17ade55f9e4736f2beba5488
nexialistamentor <mentoria.nexialista@gmail.com>
2026-01-29 20:38:07 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Recupera ST", layout="centered")
+
+st.title("Recupera ST – Diagnóstico Fiscal")
+st.write("Aplicação rodando com sucesso.")
+
